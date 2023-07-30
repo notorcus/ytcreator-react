205267c52ffbff649f1d7270d678ea07a20c2cc4
Akki <akshat.kr12090@gmail.com>
2023-07-30 12:55:35 -0400

    VideoPlayer buttons removed
    Now you can only play or pause the video by clicking on the video.
    all other stuff from the video player has been removed

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,15 +1,36 @@
-/* VideoPlayer.tsx */
-import React, { RefObject } from 'react';
+// VideoPlayer.tsx
+import React, { useEffect, useState } from 'react';
 import './VideoPlayer.css';
 
 interface VideoPlayerProps {
-  videoRef: RefObject<HTMLVideoElement>;
+  videoRef: React.RefObject<HTMLVideoElement>;
 }
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoRef }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    const handleClick = () => {
+      if (isPlaying) {
+        video.pause();
+      } else {
+        video.play();
+      }
+      setIsPlaying(!isPlaying);
+    };
+
+    video.addEventListener('click', handleClick);
+    return () => {
+      video.removeEventListener('click', handleClick);
+    };
+  }, [videoRef, isPlaying]);
+
   return (
     <div className="video-player">
-      <video ref={videoRef} controls>
+      <video ref={videoRef}>
         <source src="/MW Hormozi.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
