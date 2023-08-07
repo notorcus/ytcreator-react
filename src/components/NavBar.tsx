3088207ff79586e31a8376631865b005268c57f2
Akki <akshat.kr12090@gmail.com>
2023-08-07 18:44:45 -0400

    NavBar now highlights active page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
+  const location = useLocation();
+
   return (
     <nav className="navBar">
       <ul>
-        <li>
+        <li className={location.pathname === "/" ? "active" : ""}>
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className={location.pathname === "/edit" ? "active" : ""}>
           <Link to="/edit">Edit</Link>
         </li>
-        <li>
+        <li className={location.pathname === "/videos" ? "active" : ""}>
           <Link to="/videos">Videos</Link>
         </li>
       </ul>
